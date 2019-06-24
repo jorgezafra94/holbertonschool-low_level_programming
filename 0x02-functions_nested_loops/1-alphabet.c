4123127ffd11004904e04923436b4a84fd559e19
jorgezafra94 <947@holbertonschool.com>
2019-06-24 23:55:57 +0000

    print alphabet using _putchar function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_alphabet - writes the character alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
