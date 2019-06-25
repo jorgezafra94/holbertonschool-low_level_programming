ddd5c60a16ca0b96b087046e9d81deb5e99ef4eb
jorgezafra94 <947@holbertonschool.com>
2019-06-25 00:05:16 +0000

    function to print Military hour

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * jack_bauer - give you the time from 00:00 to 23:59
+ *
+ * Return : 0 success
+ */
+void jack_bauer(void)
+{
+	int x, y, z, w;
+
+	for (x = '0'; x <= '2'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			for (z = '0'; z <= '5'; z++)
+			{
+				for (w = '0'; w <= '9'; w++)
+				{
+					if (!(x == '2' && y > '3'))
+					{
+						_putchar(x);
+						_putchar(y);
+						_putchar(':');
+						_putchar(z);
+						_putchar(w);
+						_putchar('\n');
+					}
+					else
+					{
+						break;
+					}
+					
+				}
+			}
+		}
+	}
+}
