b1996a91eaf8bd2f5e33ca8046fe65cc895337ed
jorgezafra94 <947@holbertonschool.com>
2019-06-25 22:43:06 +0000

    finobacci number 98th

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,33 +6,39 @@
  */
 int main(void)
 {
-	long double n1 = 1;
-	long double n2 = 1;
-	int i;
-
-	long double sum;
+	long int n1 = 1;
+	long int n2 = 1;
+	int i, c;
+	long int  sum, sum1, sum2, var11, var12, var21, var22;
 
 	for (i = 0; i < 98; i++)
 	{
 		if (i == 0)
-		{
-			printf("%.0Lf, ", n1);
-		}
-		else
-		{
-			sum = n1 + n2;
+		{ printf("%.0ld, ", n1); }
+		else if (i < 91)
+		{ sum = n1 + n2;
 			n1 = n2;
 			n2 = sum;
-			if (!(i == 97))
-			{
-				printf("%.0Lf, ", sum);
-			}
+			printf("%ld, ", sum); }
+		else
+		{
+			if (i == 91)
+			{ var11 = n1 / 1000000000;
+				var12 = n1 % 1000000000;
+				var21 = n2 / 1000000000;
+				var22 = n2 % 1000000000; }
 			else
-			{
-				printf("%.0Lf", sum);
-			}
-		}
-	}
+			{ var11 = var21;
+				var12 = var22;
+				var21 = sum1;
+				var22 = sum2; }
+			sum2 = (var12 + var22) % 1000000000;
+			c = (var12 + var22) / 1000000000;
+			sum1 = var11 + var21 + c;
+			if (!(i == 97))
+				printf("%ld%ld, ", sum1, sum2);
+			else
+				printf("%ld%ld", sum1, sum2); } }
 	printf("\n");
 	return (0);
 }
