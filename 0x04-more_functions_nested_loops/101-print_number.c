34d1d9176ad83ac2c4b9f6845c3d6561e059cdef
jorgezafra94 <947@holbertonschool.com>
2019-06-27 22:24:21 +0000

    print all numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_number - print the number
+ * @n: input
+ * Return: return the number
+ */
+void print_number(int n)
+{
+	int i, j, cont = 1, var1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * (-1);
+	}
+	for (i = 10; i < n; i = i * 10)
+	{
+		cont++;
+	}
+	i = i / 10;
+	for (j = 1; j <= cont; j++)
+	{
+		var1 = n / i;
+		n = n % i;
+		i = i / 10;
+		_putchar ('0' + var1);
+	}
+}
