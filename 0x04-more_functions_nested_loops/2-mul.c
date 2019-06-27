1c6ff5aec1c54fd5f28703eb663a6920039e1cf3
jorgezafra94 <947@holbertonschool.com>
2019-06-27 14:24:15 +0000

    function of multiplication of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _mul - return de value of multiplication between a and b
+ * @a: input one
+ * @b: input two
+ * Return: the respective value of multiplication
+ */
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+
+	return (mult);
+}
