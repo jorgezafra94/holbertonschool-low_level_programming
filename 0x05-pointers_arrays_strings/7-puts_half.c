76a7b0691ce75bbdc90faa22f1c24dd34386fe3f
jorgezafra94 <947@holbertonschool.com>
2019-07-01 21:57:55 +0000

    show only the half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * puts_half - give you the half of the string
+ * @str: input
+ * Return: the half of a string
+ */
+void puts_half(char *str)
+{
+	int cont1 = 0, cont2 = 0;
+
+	while (*(str + cont1) != '\0')
+	{
+		cont1++;
+	}
+	if ((cont1 % 2) == 0)
+		cont2 = cont1 / 2;
+	else
+		cont2 = (cont1 - 1) / 2;
+
+	while (cont1 >= (cont2 + 1))
+	{
+		_putchar(*(str + cont2));
+		cont2++;
+	}
+	_putchar('\n');
+}
