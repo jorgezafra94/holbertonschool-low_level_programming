e413f8a72d48ad321862afe4377cade2908cc92a
jorgezafra94 <947@holbertonschool.com>
2019-07-01 21:55:02 +0000

    print the String in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_rev - give you the string in reverse form
+ * @s: input of a string
+ * Return: the string in reverse form
+ */
+void print_rev(char *s)
+{
+	int cont = 0;
+
+	while (*(s + cont) != '\0')
+	{
+		cont++;
+	}
+	while (cont >= 0)
+	{
+		_putchar(*(s + cont));
+		cont--;
+	}
+	_putchar('\n');
+}
