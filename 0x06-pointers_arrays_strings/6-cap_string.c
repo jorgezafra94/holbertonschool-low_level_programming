104da3582e41c3a5561d162dda8316b58919e8b2
jorgezafra94 <947@holbertonschool.com>
2019-07-04 18:08:50 +0000

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - change lowercase letter into uppercase in special cases
+ * @a: an array of chars
+ * Return: return an array with somes upper
+ */
+char *cap_string(char *a)
+{
+	int cont1 = 0, cont2;
+
+	int array[13] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	while (*(a + cont1) != '\0')
+	{
+		if (*(a + cont1) >= 97 && *(a + cont1) <= 122)
+		{
+			for (cont2 = 0; cont2 < 13; cont2++)
+			{
+				if(*(a + (cont1 - 1)) == array[cont2])
+				{
+					*(a + cont1) = (*(a + cont1)) - 32;
+				}
+			}
+		}
+		cont1++;
+	}
+	return (a);
+}
