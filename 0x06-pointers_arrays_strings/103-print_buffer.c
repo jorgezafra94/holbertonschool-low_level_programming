58ec49655842dc567c85c957ba6ccd39426d2888
jorgezafra94 <947@holbertonschool.com>
2019-07-05 02:03:45 +0000

    print buffer

---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_buffer - print the buffer in a specific way
+ * @b: buffer
+ * @size: size
+ * Return: nothing
+ */
+void print_buffer(char *b, int size)
+{
+	int i = 0, j, k, m, cont1 = 0, cont2 = 0;
+	while (i <= size)
+	{
+		if((i % 10) == 0)
+		{
+			printf("%.8x: ", i);
+			for (j = i; j < (10 + i); j = j + 2)
+			{
+				for (m = 0; m < 2; m++)
+				{
+					if (cont2 >= size)
+						printf("  ");
+					else
+					{ printf("%.2x", *(b + (j + m)));
+						cont2++; }
+				}
+				printf(" ");
+			}
+			for (k = i; k < (10 + i); k++)
+			{
+				if ( *(b + k) <= 20 && *(b + k) >= 0)
+					printf(".");
+				else
+					printf("%c", *(b + k));
+				cont1++;
+				if (cont1 >= size)
+					break;
+			}
+			printf("\n");
+		}
+		i = i + 10;
+	}
+}
