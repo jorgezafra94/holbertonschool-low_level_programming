786c62ed78bfc7e6f37526abb4415730bb053127
jorgezafra94 <947@holbertonschool.com>
2019-07-08 15:06:00 +0000

    return the specific part of array

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stddef.h>
+/**
+ * _strchr - prints from the character found
+ * @s: source array
+ * @c: character to find
+ * Return: array changed
+ */
+char *_strchr(char *s, char c)
+{
+	int cont1 = 0, cont2 = -1;
+	char *p = NULL;
+
+	while (*(s + cont1) != '\0')
+	{
+		if (s[cont1] == c)
+		{
+			cont2 = cont1;
+			break;
+		}
+		cont1++;
+	}
+	if (cont2 != -1)
+	{
+		return (s + cont2);
+	}
+	else
+	{
+		return (p);
+	}
+}
