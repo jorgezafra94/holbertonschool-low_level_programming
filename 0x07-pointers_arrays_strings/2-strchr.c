5414f7a42e5611f2b116c0889fa908cf68f565a3
jorgezafra94 <947@holbertonschool.com>
2019-07-08 23:11:09 +0000

    search character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * _strchr - prints from the character found
  * @s: source array
@@ -15,7 +14,7 @@ char *_strchr(char *s, char c)
 		{
 			return (s);
 		}
-		s++;
+		++s;
 	}
-	return (NULL);
+	return (0);
 }
