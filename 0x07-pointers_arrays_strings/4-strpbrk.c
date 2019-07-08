9d99bd9986dc96d873e0b2ef0f1ef9da0567a12e
jorgezafra94 <947@holbertonschool.com>
2019-07-08 16:03:13 +0000

    print the array from the first match

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * _strpbrk - prints from the character found
+ * @s: source array
+ * @accept: list of elements to search
+ * Return: array changed
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int cont1 = 0, cont2 = 0, aux = 0, prueba = 0;
+
+	while (*(s + cont1) != '\0')
+	{
+		cont2 = 0;
+		while (*(accept + cont2) != '\0')
+		{
+			if (s[cont1] == accept[cont2])
+			{
+				aux++;
+				prueba = 1;
+				break;
+			}
+			cont2++;
+		}
+		if (prueba == 1)
+			break;
+		cont1++;
+	}
+	if ( prueba != 0)
+		return (s + cont1);
+	else
+		return (0);
+}
