6c95e75031562a1ccb72f18a6d80bc4efc75f267
jorgezafra94 <947@holbertonschool.com>
2019-07-08 18:28:24 +0000

    print the string found

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * _strstr - prints from the character found
+ * @haystack: source array
+ * @needle: list of elements to search
+ * Return: array
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int cont1 = 0, aux = 0, prueba = 0, ini = 0, cont2 = 0;
+
+	while (*(needle + aux) != '\0')
+		aux++;
+	while (*(needle + cont1) != '\0')
+	{
+		while (*(haystack + cont2) != '\0')
+		{
+			if (haystack[cont2] == needle[cont1])
+			{
+				cont1++;
+				cont2++;
+				prueba++;
+				if (prueba == aux)
+				{
+					ini = cont2 - aux;
+				}
+			}
+			else
+			{
+				cont1 = 0;
+				cont2++;
+			}
+		}
+	}
+	if (prueba == aux)
+		return (haystack + ini);
+	else
+		return (0);
+}
