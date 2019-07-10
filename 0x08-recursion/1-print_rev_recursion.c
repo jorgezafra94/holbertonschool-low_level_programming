a947ff83b38b74f47a54fbdf8185d4c55d3f74df
jorgezafra94 <947@holbertonschool.com>
2019-07-10 17:05:33 +0000

    print rev of string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - prints the strings in reverse form
+ * @s: string input
+ */
+void _print_rev_recursion(char *s)
+{
+	if (s[1] != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar(s[0]);
+	}
+}
