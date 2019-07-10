8993e4f24573a09912898a271001d4dd7f8f27f1
jorgezafra94 <947@holbertonschool.com>
2019-07-10 17:17:29 +0000

    pow of a number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,12 +8,16 @@
 int _pow_recursion(int x, int y)
 {
 	if (y == 0)
+	{
 		return (1);
+	}
 	else if (y < 0)
+	{
 		return (-1);
+	}
 	else
 	{
 		--y;
-		return (x * _pow_recursion(x , y));
+		return (x * _pow_recursion(x, y));
 	}
 }
