b2f50d1c6da0374df22af4ae0ec939a9f9b92a15
jorgezafra94 <947@holbertonschool.com>
2019-07-16 21:06:07 +0000

    print the arguments

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * argstostr - create a matrix from arguments
+ * @ac: number of arguments
+ * @av: array of pointers
+ * Return: return a pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	int j = 0, i = 0, cont = 0, aux = 0;
+	char *p;
+
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			cont++;
+			j++;
+		}
+		cont++;
+		i++;
+	}
+/* count the number of chars including the nulls */
+	p = malloc(sizeof(char) * (cont + 1));
+	if (!p)
+	{
+		free(p);
+		return (NULL);
+	}
+	i = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			p[aux] = av[i][j];
+			j++;
+			aux++;
+		}
+		p[aux] = '\n';
+		aux++;
+		i++;
+	}
+/* asign the value of each char and the null replace by \n */
+	p[aux] = '\0';
+	return (p);
+}
