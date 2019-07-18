18dac9815239647b6f017fe6e97c9836debd8c72
jorgezafra94 <947@holbertonschool.com>
2019-07-18 17:56:08 +0000

    concatenate 2 strings especially

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,21 +9,17 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int cont1 = 0, cont2 = 0, i, aux = 0;
+	unsigned int cont1 = 0, cont2 = 0, i;
 	char *p = NULL;
 
-	if (s1 == NULL)
+	/*if (s1 == NULL)
 	{
 		s1 = "";
-		aux++;
-	}
+	}*/
 	if (s2 == NULL)
 	{
 		s2 = "";
-		aux++
 	}
-	if (aux == 2)
-		return (NULL);
 	if (n == 0)
 		return (NULL);
 	while (s1[cont1] != '\0')
