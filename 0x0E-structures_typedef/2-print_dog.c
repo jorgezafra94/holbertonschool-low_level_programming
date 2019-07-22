7095f59bf329217f599c4dce22b54c5d1d7a0357
jorgezafra94 <947@holbertonschool.com>
2019-07-22 17:41:38 +0000

    print members of a dog variable

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,6 +8,7 @@ void print_dog(struct dog *d)
 {
 	if (d == 0)
 	{
+		return;
 	}
 	else
 	{
@@ -15,10 +16,7 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", (*d).name);
-		if ((*d).age == 0.0)
-			printf("Age: (nil)\n");
-		else
-			printf("Age: %f\n", (*d).age);
+		printf("Age: %f\n", (*d).age);
 		if ((*d).owner == 0)
 			printf("Owner: (nil)\n");
 		else
