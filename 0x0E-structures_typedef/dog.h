3262eddc91b8c9262935288384ee3504623a7ad7
jorgezafra94 <947@holbertonschool.com>
2019-07-22 15:38:03 +0000

    structure in a header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef ZETA
+#define ZETA
+
+/**
+ * struct dog - create the structure of a dog
+ * @name: First element or member
+ * @age: Second member
+ * @owner: third member
+ * Description: main members of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
