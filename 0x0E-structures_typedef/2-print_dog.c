3456b611095a8376c32adbe8663cc0c858796cc1
jorgezafra94 <947@holbertonschool.com>
2019-07-22 16:19:51 +0000

    print information of a dog variable

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - print the values of the structure dog or nil
+ * @d: variable of type dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d == 0)
+	{
+	}
+	else
+	{
+		if ((*d).name == 0)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", (*d).name);
+		if ((*d).age == 0.0)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", (*d).age);
+		if ((*d).owner == 0)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", (*d).owner);
+	}
+}
