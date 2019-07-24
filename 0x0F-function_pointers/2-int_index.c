717ece8fcdc4f3e497312ffc2059f3ab25d4e893
jorgezafra94 <947@holbertonschool.com>
2019-07-24 17:51:56 +0000

    [fix] first conditional

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,13 +11,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, res = -1;
 
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		res = cmp(array[i]);
-		if (res != 0)
-			break;
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			res = cmp(array[i]);
+			if (res != 0)
+				break;
+		}
 	}
 	if (res == 1)
 	{
