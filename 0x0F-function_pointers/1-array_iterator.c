113ca46447366e5c855767ac784313945132ecc4
jorgezafra94 <947@holbertonschool.com>
2019-07-24 17:38:57 +0000

    [fix] if input is null

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,15 +1,15 @@
 #include "function_pointers.h"
 /**
  * array_iterator - print elements in dec or in hex
- * @array: input char
- * @size: function pointer
+ * @array: input array
+ * @size: size of array
  * @action: function pointer, depending of the input choose dec or hex
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array || action)
+	if (action && size <= 0)
 	{
 		for (i = 0; i < size; i++)
 		{
