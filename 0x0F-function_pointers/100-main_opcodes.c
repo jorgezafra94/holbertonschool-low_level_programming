f5f4013a9229df13533620e5d5c7df91069fc19f
jorgezafra94 <947@holbertonschool.com>
2019-07-25 00:29:48 +0000

    opcode of main

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,18 +9,19 @@
 int main(int argc, char *argv[])
 {
 	int i, aux;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return(1);
+		return (1);
 	}
 	aux = atoi(argv[1]);
-	if (aux <= 0)
+	if (aux < 0)
 	{
 		printf("Error\n");
-		return(2);
+		return (2);
 	}
-	for (i = 0; i < aux; i++ )
+	for (i = 0; i < aux; i++)
 	{
 		printf("%02x", ((unsigned char *)main)[i]);
 		printf(" ");
