f88dd5c07a8ce82c9580e4de993168a87f01fb8a
jorgezafra94 <947@holbertonschool.com>
2019-07-25 00:26:00 +0000

    opcode of main

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - printf byte per byte of main direction
+ * @argc: number of arguments
+ * @argv: arguments in the input
+ * Return: direction of main function
+ */
+int main(int argc, char *argv[])
+{
+	int i, aux;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return(1);
+	}
+	aux = atoi(argv[1]);
+	if (aux <= 0)
+	{
+		printf("Error\n");
+		return(2);
+	}
+	for (i = 0; i < aux; i++ )
+	{
+		printf("%02x", ((unsigned char *)main)[i]);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
