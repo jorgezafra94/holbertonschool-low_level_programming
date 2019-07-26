b28d78592be5bb23666837c601873976e978a0ee
jorgezafra94 <947@holbertonschool.com>
2019-07-26 18:18:09 +0000

    variadic function print a separator and number given

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - print all the numbers followed by a separator
+ * @separator: will separate the elements
+ * @n: number of elements
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list elements;
+	unsigned int i;
+	int num;
+
+	if (n == 0)
+	{
+		va_end(elements);
+		return;
+	}
+	va_start(elements, n);
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(elements, int);
+		if (!(i == (n - 1)))
+		{
+			if (separator != NULL)
+				printf("%d%s", num, separator);
+			else
+				printf("%d", num);
+		}
+		else
+		{
+			printf("%d\n", num);
+		}
+	}
+	va_end(elements);
+}
