7a7eb128d611897bb877f19b105c67f947c9499c
jorgezafra94 <947@holbertonschool.com>
2019-07-26 16:07:56 +0000

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,9 @@
 #ifndef ZETA
 #define ZETA
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
