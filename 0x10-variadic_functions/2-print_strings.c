c85a4cdb0ef2b4103d1b83485db0bdeb9e89b804
jorgezafra94 <947@holbertonschool.com>
2019-07-26 18:45:59 +0000

    variadic function that print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - print all the words followed by a separator
+ * @separator: will separate the elements
+ * @n: number of elements
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list selements;
+	unsigned int i;
+	char *nom;
+
+	if (n == 0)
+	{
+		va_end(selements);
+		return;
+	}
+	va_start(selements, n);
+	for (i = 0; i < n; i++)
+	{
+		nom = va_arg(selements, char *);
+		if (nom == NULL)
+			nom = "(nil)";
+		if (!(i == (n - 1)))
+		{
+			if (separator != NULL)
+				printf("%s%s", nom, separator);
+			else
+				printf("%s", nom);
+		}
+		else
+		{
+			printf("%s\n", nom);
+		}
+	}
+	va_end(selements);
+}
