71556fc774ab5ce5dd459496e4f0b739653230a7
jorgezafra94 <947@holbertonschool.com>
2019-07-27 15:52:59 +0000

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * struct op- Struct op
+ *
+ * @c: The character
+ * @fun: The function associated
+ */
+typedef struct op
+{
+	char c;
+	void (*fun)(va_list);
+} lista;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
