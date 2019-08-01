a7555c520ea3b905cf6739c47467fab5c97c0d2e
jorgezafra94 <947@holbertonschool.com>
2019-08-01 20:20:28 +0000

    [Feature] 3-add_node_end.c: add a new element before the NULL element of the linked list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * add_node_end - add a new element before the NULL
+ * @head :linked list
+ * @str: new string to add
+ * Return: return the list but with the new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp, *aux;
+	int c = 0;
+
+	temp = malloc(sizeof(list_t));
+	if (!temp)
+		return (NULL);
+
+	temp->str = strdup(str);
+
+	while (str[c] != '\0')
+		c++;
+
+	temp->len = c;
+
+	if (*head == NULL)
+	{
+		temp->next = *head;
+		*head = temp;
+	}
+	else
+	{
+		aux = *head;
+		while (aux->next != NULL)
+		{
+			aux = aux->next;
+		}
+		temp->next = aux->next;
+		aux->next = temp;
+	}
+	return (aux);
+}
