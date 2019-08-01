9ffcae9f86cef13aafa5f8551624a7ad86356c66
jorgezafra94 <947@holbertonschool.com>
2019-08-01 21:08:30 +0000

    [feature] 100-first: print the phrase before the main, using attribute constructor in header

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * print_phrase - printf the phrase before the main review the header
+ */
+void print_phrase(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
