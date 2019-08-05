0981bdb760f013164592527d21933645fa622664
jorgezafra94 <947@holbertonschool.com>
2019-08-05 16:00:37 +0000

    [Feature] 6-poop_listint.c: delete the first element of the linked list and return its member n

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * pop_listint - add a node at the end of the linked list
+ * @head: pointer to a pointer taht point to linked list
+ * Return: a pointer to the linked list
+ */
+int pop_listint(listint_t **head)
+{
+	int a = 0;
+	listint_t *aux;
+
+/*when get the last element*/
+	if ((*head)->next == NULL)
+	{
+		a = (*head)->n;
+		free(*head);
+	}
+	/* all the other previous elements*/
+	else
+	{
+		aux = *head;
+		a = aux->n;
+		*head = (*head)->next;
+		free(aux);
+	}
+	return (a);
+
+}
