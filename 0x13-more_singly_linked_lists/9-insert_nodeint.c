19a1d495183e9834c3b7cff232e0cc5cb831faf7
jorgezafra94 <947@holbertonschool.com>
2019-08-05 23:04:45 +0000

    [Feature] 9-insert_nodeint.c: add a new node in the index position of the linked list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - add the node in the idx
+ * position in the linked list
+ * @head: pointer to a pointer that points to linked list
+ * @idx:position to add the new node
+ * @n: value of memeber n of the new node;
+ * Return: a node of the linked list
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int cont = 1;
+	listint_t *aux, *p;
+
+	aux = *head;
+	for (; aux->next != NULL; cont++)
+		aux = aux->next;
+	if (cont < idx)
+		return (NULL);
+	p = malloc(sizeof(listint_t));
+	if (!p)
+		return (NULL);
+	p->n = n;
+	if (*head == NULL || idx == 0)
+	{
+		p->next = *head;
+		*head = p;
+		return (*head);
+	}
+	cont = 1;
+	aux = *head;
+	while (aux->next != NULL)
+	{
+		if (cont == idx)
+			break;
+		cont++;
+		aux = aux->next;
+	}
+	p->next = aux->next;
+	aux->next = p;
+	return (*head);
+}
