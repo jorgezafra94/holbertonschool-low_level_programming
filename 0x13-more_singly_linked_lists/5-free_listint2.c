5fed12554eaf93f9a4757e489be09b33ebd703ad
jorgezafra94 <947@holbertonschool.com>
2019-08-05 15:40:53 +0000

    [feature] 5-fre_listint2.c: free the structure using a double pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - free each element of the linked list
+ * @head: pointer to a pointer that point to the linked list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *aux;
+
+	if (*head == NULL)
+	{
+		free(head);
+		return;
+	}
+	while (*head != NULL)
+	{
+		aux = *head;
+		*head = (*head)->next;
+		free(aux);
+	}
+
+}
