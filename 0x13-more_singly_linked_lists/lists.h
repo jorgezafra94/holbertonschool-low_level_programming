9dac2e7250a20515e6fb4413da8042b5fa2ab02e
jorgezafra94 <947@holbertonschool.com>
2019-08-07 00:32:55 +0000

    update add function free_listp

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -45,4 +45,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+void free_listp(listp_t *p);
 #endif
