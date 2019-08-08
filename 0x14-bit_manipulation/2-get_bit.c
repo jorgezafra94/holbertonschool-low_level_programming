925b4fd3e4ec7dc189328c3ba6af096e5382488e
jorgezafra94 <947@holbertonschool.com>
2019-08-08 17:51:30 +0000

    [Feature]: function that get the bit in index position

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * get_bit - search the bit in a binary
+ * @n: number in decimal
+ * @index: position that starts in 0
+ * Return: the bit in the index specified
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i = 0, j = 0;
+	unsigned long int cont;
+	int aux = 0;
+
+	cont = n;
+	if (n == 0)
+	{
+		return (0);
+	}
+	while (cont != 0)/*counting number of elements*/
+	{
+		j++;
+		cont = cont >> 1;
+	}
+	if (j <= index)/*evaluate if the index is bigger than amount numbers*/
+	{
+		return (-1);
+	}
+	else
+	{
+		for (; i < index; i++) /*go through the numbers*/
+			n = n >> 1;
+		aux = n & 1;/*get the bit in index number*/
+		return (aux);
+	}
+}
