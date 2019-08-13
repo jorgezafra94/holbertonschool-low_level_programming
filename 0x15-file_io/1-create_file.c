09b5a7978bb76b09c7161c900be6846a2e94d236
jorgezafra94 <947@holbertonschool.com>
2019-08-13 17:45:43 +0000

    [fix] if write fails

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int x, c = 0;
+	int x, c = 0, fa;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,7 +22,9 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[c] != '\0')
 		c++;
 
-	write(x, text_content, c);
+	fa = write(x, text_content, c);
+	if (fa == -1)
+		return (-1);
 
 	close(x);
 	return (1);
