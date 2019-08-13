cfe89c47dcf03a6a8ef68869da9628381ed10370
jorgezafra94 <947@holbertonschool.com>
2019-08-13 17:52:05 +0000

    [fix] truncate if the file already exist

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	/*printf("%d, %d\n", O_CREAT, O_WRONLY);*/
 
-	x = open(filename, O_CREAT | O_WRONLY, 0600);
+	x = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (text_content == NULL)
 		text_content = "";
