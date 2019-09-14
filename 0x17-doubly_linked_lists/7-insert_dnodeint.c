a20a0eeb4a03304d0ee6a5b2f75769a65b4be37d
jorgezafra94 <947@holbertonschool.com>
2019-09-14 18:09:07 +0000

    [fix] correction of the first statement

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp, *aux;
 	unsigned int num = 0;
 
-	if (h == NULL)
+	if (h == NULL || ((*h == NULL) && idx != 0))
 		return (NULL);
 	temp = malloc(sizeof(dlistint_t));
 	if (!temp)
