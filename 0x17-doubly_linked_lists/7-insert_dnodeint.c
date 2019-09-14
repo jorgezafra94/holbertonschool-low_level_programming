bd6841033b52628ddb2e380a64cffd94a5c674bb
jorgezafra94 <947@holbertonschool.com>
2019-09-14 23:14:45 +0000

    return NULL at the end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -45,6 +45,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp->next = NULL;
 		temp->prev = aux;
 		aux->next = temp;
+		return (temp);
 	}
-	return (temp);
+	return (NULL);
 }
