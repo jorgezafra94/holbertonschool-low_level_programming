090780d22f22ea5ff484c1c3ebc26dfec8cbd7c1
jorgezafra94 <947@holbertonschool.com>
2019-09-14 22:32:46 +0000

    [fix] addign first statement

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,37 +11,36 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp = NULL, *aux = NULL;
 	unsigned int num = 0;
 
-	if (!(h == NULL || (*h == NULL && idx != 0)))
+	if (h == NULL || (*h == NULL && idx != 0))
+		return (temp);
+	if (idx == 0)
+		temp = add_dnodeint(h, n);
+	else
 	{
-		if (idx == 0)
-			temp = add_dnodeint(h, n);
-		else
+		temp = malloc(sizeof(dlistint_t));
+		if (!temp)
+			return (NULL);
+		temp->n = n;
+		aux = *h;
+		while (aux->next)
 		{
-			temp = malloc(sizeof(dlistint_t));
-			if (!temp)
-				return (NULL);
-			temp->n = n;
-			aux = *h;
-			while (aux->next)
-			{
-				if (idx == num)
-				{
-					temp->prev = aux->prev;
-					temp->next = aux;
-					temp->prev->next = temp;
-					temp->next->prev = temp;
-					break;
-				}
-				num++;
-				aux = aux->next;
-			}
-			num++;
 			if (idx == num)
 			{
-				temp->prev = aux;
-				temp->next = NULL;
+				temp->prev = aux->prev;
+				temp->next = aux;
 				temp->prev->next = temp;
+				temp->next->prev = temp;
+				break;
 			}
+			num++;
+			aux = aux->next;
+		}
+		num++;
+		if (idx == num)
+		{
+			temp->prev = aux;
+			temp->next = NULL;
+			temp->prev->next = temp;
 		}
 	}
 	return (temp);
