9657e973cf19fa20cef6c461e4b585629e6075b0
jorgezafra94 <947@holbertonschool.com>
2019-09-16 22:31:45 +0000

    [fix] store the file in tmp

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P tmp/ https://raw.github.com/jorgezafra94/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/jack.so
-export LD_PRELOAD=tmp/jack.so
+wget -P /tmp/ https://raw.github.com/jorgezafra94/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/jack.so
+export LD_PRELOAD=/tmp/jack.so
