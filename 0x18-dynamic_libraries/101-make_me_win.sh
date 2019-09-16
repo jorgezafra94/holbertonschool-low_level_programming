9b217d82f9469d3a8d4ffd6b39e0d12044428cbe
jorgezafra94 <947@holbertonschool.com>
2019-09-16 22:25:34 +0000

    [Fix] import file .so into tmp/ dir

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-export LD_PRELOAD=$PWD/jack.so
+wget -P tmp/ https://raw.github.com/jorgezafra94/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/jack.so
+export LD_PRELOAD=tmp/jack.so
