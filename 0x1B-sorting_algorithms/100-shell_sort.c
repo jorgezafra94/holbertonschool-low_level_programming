65b6325c7b22672b0d66d6a0b581d66fcb049256
jorgezafra94 <947@holbertonschool.com>
2019-10-28 18:37:57 +0000

    [fix] shell sorting

--- 0x1B-sorting_algorithms/100-shell_sort.c
+++ 0x1B-sorting_algorithms/100-shell_sort.c
@@ -6,26 +6,25 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	int aux, cont = 0;
-	size_t i, j, h = 1;
+	size_t h = 1, i, j;
+	int aux = 0;
 
 	while (h < (size / 3))
+	{
 		h = (3 * h) + 1;
-
+	}
 	while (h > 0)
 	{
-		for (i = h; i < size; i++)
+		for (i = 0; (i + h) < size; i++)
 		{
-			for (j = i; array[j] < array[j - h]; j = j - h)
+			for (j = i + h; array[j] < array[j - h]; j -= h)
 			{
-				cont++;
 				aux = array[j];
 				array[j] = array[j - h];
 				array[j - h] = aux;
 			}
 		}
 		h = (h - 1) / 3;
-		if (cont != 0)
-			print_array(array, size);
+		print_array(array, size);
 	}
 }
