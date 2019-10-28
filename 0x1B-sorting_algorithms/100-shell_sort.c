d93c981de8e43a31ff8fd3f39ff20cf1c05c9fb1
jorgezafra94 <947@holbertonschool.com>
2019-10-28 19:20:36 +0000

    [fix] while of h

--- 0x1B-sorting_algorithms/100-shell_sort.c
+++ 0x1B-sorting_algorithms/100-shell_sort.c
@@ -6,25 +6,26 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t h = 1, i, j;
+	int h = 1, i, j;
 	int aux = 0;
 
-	while (h < (size / 3))
+	while (h < ((int)(size)))
 	{
 		h = (3 * h) + 1;
 	}
+	h = (h - 1) / 3;
 	while (h > 0)
 	{
-		for (i = 0; (i + h) < size; i++)
+		for (i = h; i < (int)(size); i++)
 		{
-			for (j = i + h; array[j] < array[j - h]; j -= h)
+			for (j = i; (j - h) >= 0 && array[j] < array[j - h]; j -= h)
 			{
 				aux = array[j];
 				array[j] = array[j - h];
 				array[j - h] = aux;
 			}
 		}
-		h = (h - 1) / 3;
 		print_array(array, size);
+		h = (h - 1) / 3;
 	}
 }
