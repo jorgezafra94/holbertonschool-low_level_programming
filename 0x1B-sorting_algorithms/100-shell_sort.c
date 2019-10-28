ff2255dc4988f11f79e0392ec2a309c49f4f2486
jorgezafra94 <947@holbertonschool.com>
2019-10-28 17:20:17 +0000

    [feature] shell sort array

---
+++ 0x1B-sorting_algorithms/100-shell_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * shell_sort - sort the array using shell method and knuth sequence
+ * @array: array to sort
+ * @size: size of array
+ */
+void shell_sort(int *array, size_t size)
+{
+	int aux;
+	size_t h = 1, i, j;
+
+	while (h < (size / 3))
+	{
+		h = (3 * h) + 1;
+	}
+
+	while (h > 0)
+	{
+		for (i = h; i < size; i++)
+		{
+			for (j = i; j > 0 && array[j] < array[j - h]; j = j - h)
+			{
+				aux = array[j];
+				array[j] = array[j - h];
+				array[j - h] = aux;
+			}
+		}
+		h = (h - 1) / 3;
+		print_array(array, size);
+	}
+}
