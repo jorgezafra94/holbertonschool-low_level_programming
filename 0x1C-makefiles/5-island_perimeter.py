aa0a2d0988905e05144df688fc252cf087534633
jorgezafra94 <947@holbertonschool.com>
2019-11-15 20:05:33 +0000

    [fix] no exceed 100

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,20 +8,27 @@ def island_perimeter(grid):
     1 represents a land zone
     """
     count = 0
-    for row in range(len(grid)):
-        if row <= 100:
-            for cell in range(len(grid[row])):
-                if grid[row][cell] == 1 and len(grid[row]) < 100:
-                    top = row - 1
-                    down = row + 1
-                    left = cell - 1
-                    right = cell + 1
-                    if top >= 0 and grid[top][cell] == 0:
+    flag = 0
+    if len(grid) > 100:
+        flag = 1
+    else:
+        for elem in grid:
+            if len(elem) > 100:
+                flag = 1
+    if flag == 0:
+        for row in range(len(grid)):
+            for c in range(len(grid[row])):
+                if grid[row][c] == 1:
+                    t = row - 1
+                    d = row + 1
+                    l = c - 1
+                    r = c + 1
+                    if t >= 0 and t < len(grid[t]) and grid[t][c] == 0:
                         count = count + 1
-                    if down < len(grid) and grid[down][cell] == 0:
+                    if d < len(grid) and d < len(grid[d]) and grid[d][c] == 0:
                         count = count + 1
-                    if left >= 0 and grid[row][left] == 0:
+                    if l >= 0 and grid[row][l] == 0:
                         count = count + 1
-                    if right < len(grid[row]) and grid[row][right] == 0:
+                    if r < len(grid[row]) and grid[row][r] == 0:
                         count = count + 1
-            return (count)
+    return (count)
