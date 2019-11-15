2f8c4f06c3a279ac387c0fe9fb883dd91c18800d
jorgezafra94 <947@holbertonschool.com>
2019-11-15 21:03:31 +0000

    [feature] island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,17 +14,12 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                top = row - 1
-                down = row + 1
-                left = col - 1
-                right = col + 1
-                if top >= 0 and grid[top][col] == 0:
+                if row == 0 or grid[row - 1][col] == 0:
                     count = count + 1
-                if down < len(grid) and grid[down][col] == 0:
+                if row == len(grid) or grid[row + 1][col] == 0:
                     count = count + 1
-                if left >= 0 and grid[row][left] == 0:
+                if col == 0 or grid[row][col - 1] == 0:
+                    count =count + 1
+                if col == len(grid[row]) or grid[row][col + 1] == 0:
                     count = count + 1
-                if right < len(grid[row]) and grid[row][right] == 0:
-                    count = count + 1
-
     return (count)
