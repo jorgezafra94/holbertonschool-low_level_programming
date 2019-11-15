4dea32b648955f3383dd678d17ff5bce77a05cb6
jorgezafra94 <947@holbertonschool.com>
2019-11-15 19:54:22 +0000

    [fix] no exceed 100

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,18 +9,19 @@ def island_perimeter(grid):
     """
     count = 0
     for row in range(len(grid)):
-        for cell in range(len(grid[row])):
-            if grid[row][cell] == 1:
-                top = row - 1
-                down = row + 1
-                left = cell - 1
-                right = cell + 1
-                if top >= 0 and grid[top][cell] == 0:
-                    count = count + 1
-                if down < len(grid) and grid[down][cell] == 0:
-                    count = count + 1
-                if left >= 0 and grid[row][left] == 0:
-                    count = count + 1
-                if right < len(grid[row]) and grid[row][right] == 0:
-                    count = count + 1
-    return (count)
+        if row <= 100:
+            for cell in range(len(grid[row])):
+                if grid[row][cell] == 1 and len(grid[row]) < 100:
+                    top = row - 1
+                    down = row + 1
+                    left = cell - 1
+                    right = cell + 1
+                    if top >= 0 and grid[top][cell] == 0:
+                        count = count + 1
+                    if down < len(grid) and grid[down][cell] == 0:
+                        count = count + 1
+                    if left >= 0 and grid[row][left] == 0:
+                        count = count + 1
+                    if right < len(grid[row]) and grid[row][right] == 0:
+                        count = count + 1
+            return (count)
