d8dd7ef33053c4213df6174f1588f1e4423a3afe
= <=>
2020-01-15 16:54:01 +0000

    header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,64 @@
+#ifndef SEARCH_ALGORITHMS
+#define SEARCH_ALGORITHMS
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
+/** helper functions **/
+
+void free_list(listint_t *list);
+listint_t *create_list(int *array, size_t size);
+void print_list(const listint_t *list);
+
+void free_skiplist(skiplist_t *list);
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+
+#endif
